223a0ef67005765902d46b938f9d5e8bc7db5404
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2021-11-23 01:24:47 -0300

    Kingfisher - update run.js

--- kingfisher/scripts/run.js
+++ kingfisher/scripts/run.js
@@ -1,27 +1,25 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
-
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
 
-  console.log("Contract deployed to:", waveContract.address);
-  console.log("Contract deployed by:", owner.address);
-
-	let waveCount;
+  let waveCount;
   waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-	// simulate owner sending a wave
-	let waveTxn = await waveContract.wave("Hola mundo!");
-  await waveTxn.wait();
+  /**
+   * Let's send a few waves!
+   */
+  let waveTxn = await waveContract.wave('A message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-	// simulate random person sending a wave
-	waveTxn = await waveContract.connect(randomPerson).wave("probando");
-  await waveTxn.wait();
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-	waveMessage = await waveContract.getMessageAddress(randomPerson.address);
-  waveCount   = await waveContract.getTotalWaves();
-
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
