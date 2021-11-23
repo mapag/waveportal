7d905d0f9d71cf67596e4452727daf4b4731465f
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2021-11-23 11:49:55 -0300

    👑 - Give price to RANDOM wavers

--- kingfisher/contracts/WavePortal.sol
+++ kingfisher/contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
 
@@ -17,8 +18,9 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor () payable {
+    constructor() payable {
         console.log("Initializing Wave Portal smart contract");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _waveMessage) public {
@@ -26,15 +28,26 @@ contract WavePortal {
         console.log("%s has waved!", msg.sender);
 
         waves.push(Wave(msg.sender, _waveMessage, block.timestamp));
-        emit NewWave(msg.sender, block.timestamp, _waveMessage);
 
-        uint256 prizeAmount = 0.0001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random seed generated: %s !", seed);
+
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _waveMessage);
     }
 
     function getTotalWaves() public view returns (uint256) {
