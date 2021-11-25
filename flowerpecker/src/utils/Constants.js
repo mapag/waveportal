aee25b943f8eb6f787389a7a3938c031225b2f43
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2021-11-25 02:37:29 -0300

    🌼 - Update contract

--- flowerpecker/src/utils/Constants.js
+++ flowerpecker/src/utils/Constants.js
@@ -1,6 +1,6 @@
 const ABI = require('./WavePortal.json');
 
-const CONTRACT_ADDRESS = '0x1e52c4e2496D27E38E9880BaA55fb72A76841e46';
+const CONTRACT_ADDRESS = '0xd75ec8342ce00Ceb7E9A60306f23Df72dFbf11Ea';
 const CONTRACT_ABI = ABI.abi;
 
 module.exports = {
